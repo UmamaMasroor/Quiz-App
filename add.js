b8d27c270170631745c09f3f4b8ec6e1fd9b6146
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 20:58:13 -0700

    adding questions

--- add.js
+++ add.js
@@ -36,39 +36,39 @@ const questions = [
         ]
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?" ,
+        question: "Which programming tool was developed in 1993 and become the standard for the internet?" ,
         answers: [
-            {text: "<js>", correct: false},
-            {text: "<script>", correct: true},
-            {text: "<scripting>", correct: false},
-            {text: "<JavaScript>", correct: false},
+            {text: "CSS", correct: false},
+            {text: "JS", correct: false},
+            {text: "XML", correct: false},
+            {text: "HTML", correct: true},
         ]
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?" ,
+        question: "HTML was first proposed in year ____?" ,
         answers: [
-            {text: "<js>", correct: false},
-            {text: "<script>", correct: true},
-            {text: "<scripting>", correct: false},
-            {text: "<JavaScript>", correct: false},
+            {text: "1990", correct: true},
+            {text: "1980", correct: false},
+            {text: "1995", correct: false},
+            {text: "2000", correct: false},
         ]
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?" ,
+        question: " Basic fundamental block is called as_____ ?" ,
         answers: [
-            {text: "<js>", correct: false},
-            {text: "<script>", correct: true},
-            {text: "<scripting>", correct: false},
-            {text: "<JavaScript>", correct: false},
+            {text: "HTML tag", correct: true},
+            {text: "HTML body", correct: false},
+            {text: "HTML Attribute", correct: false},
+            {text: "HTML Element", correct: false},
         ]
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?" ,
+        question: "Why so JavaScript and Java have similar name?" ,
         answers: [
-            {text: "<js>", correct: false},
-            {text: "<script>", correct: true},
-            {text: "<scripting>", correct: false},
-            {text: "<JavaScript>", correct: false},
+            {text: "JavaScript is a stripped-down version of Java", correct: false},
+            {text: "They both originated on the island of Java", correct: false},
+            {text: "JavaScript's syntax is loosely based on Java's", correct:true},
+            {text: "None of the above", correct: false},
         ]
     },
     {
