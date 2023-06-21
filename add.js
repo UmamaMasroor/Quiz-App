1499b3e552711af3c26a7c67e673909950ddd23f
Umama Masroor <umamafatima319@gmail.com>
2023-06-20 20:34:06 -0700

    adding questions

---
+++ add.js
@@ -0,0 +1,92 @@
+const questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+    {
+        question: "How do you declare a JavaScript variable?" ,
+        answers: [
+            {text: "variable firstName", correct: false},
+            {text: "variable firstname", correct: false},
+            {text: "var firstName", correct: true},
+            {text: "None of the above", correct: false},
+        ]
+    },
+    {
+        question: "HTML abbreviation?" ,
+        answers: [
+            {text: "Home Tool Markup Language", correct: false},
+            {text: "Hyperspeed Tensible Markup Language", correct: false},
+            {text: "HyperText Markup Language", correct: true},
+            {text: "Hyperlinks and Text Markup Language", correct: false},
+        ]
+    },
+    {
+        question: "Which language runs in a web browser?" ,
+        answers: [
+            {text: "Java", correct: false},
+            {text: "JavaScript", correct: true},
+            {text: "CSS", correct: false},
+            {text: "HTML", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?" ,
+        answers: [
+            {text: "<js>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<scripting>", correct: false},
+            {text: "<JavaScript>", correct: false},
+        ]
+    },
+]
